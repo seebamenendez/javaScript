7fc4b5205aa41f41f5db395623e7bdd95b3726e6
sebamenendez <seeba.menendez@gmail.com>
2021-09-15 20:12:06 -0300

    se agrega de cero

---
+++ main.js
@@ -0,0 +1,16 @@
+let nombreIngresado = prompt("Ingrese su nombre:");
+let saludo = "¡Hola " + nombreIngresado +"!"; 
+console.log(saludo);
+
+var numero1 = 10;
+let numeroIngresado = parseInt (prompt("Ingrese un numero:"));
+let resultado = numero1 + numeroIngresado;
+console.log("El resultado de la suma es: " + resultado);
+
+        
+let texto1 = prompt("Ingrese su texto:");
+let texto2 = prompt("Ingrese otro texto:");
+
+alert (texto1 + " " + texto2);
+let resultadoTexto = texto1 + texto2
+
